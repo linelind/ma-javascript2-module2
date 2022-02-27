f485385863450be1a2a87459a302e1450028a794
linelind <line.toresby@gmail.com>
2022-02-27 11:18:45 +0100

    updated js to remove items.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,47 @@
+// create list
+
 function createList() {
     const listContainer = document.querySelector("ul");
+    const container = document.querySelector(".listContainer");
 
     listContainer. innerHTML = "";
 
     if (books.length === 0) {
-        listContainer.innerHTML = "<p>There are no more elements in the list.</p>";
-    }
+        container.innerHTML = `<p class="message">There are no more books in the list.</p>`;
+    }  
 
-    books.forEach(function (item) {
-         listContainer.innerHTML += `<li>ISBN: ${item.isbn}, a ${item.title} <i class="fa fa-trash"></i></li>`; 
+    books.forEach(function (listItem) {
+         listContainer.innerHTML += `<li><span>ISBN: ${listItem.isbn}, a ${listItem.title}</span> <i class="fa fa-trash" data-id="${listItem.isbn}"></i></li>`; 
     });
 
     const trashCans = document.querySelectorAll("li i");
 
     trashCans.forEach(function (can) {
         can.addEventListener("click", removeFromList);
-    });
+    }); 
 
-}
-
-function removeFromList() {
-
-    const deleteBook = event.target.offsetParent;
-
-    const newList = books.filter(function (item) {
-        if (deleteBook !== item) {
-            return true;
-        }
-    });
-
-    books = newList;
 }
 
 window.onload = function() {
     createList();
   };
+
+
+// filter
+
+function removeFromList() {
+
+    const id = event.target.dataset.id;
+    console.log(event.target.dataset.id);
+
+    const newList = books.filter(function (listItem) {
+        if(id !== listItem.isbn) {
+            return true;
+        }
+    
+    });
+
+    books = newList;
+
+    createList();
+}
